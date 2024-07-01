d5ca5ee263402fc87b964767e303c741b7cd8761
LeszekSarepski <149314274+LeszekSarepski@users.noreply.github.com>
2024-07-01 21:43:27 +0200

    Delete keyGenerate.py

--- keyGenerate.py
+++
@@ -1,8 +0,0 @@
-from cryptography.fernet import Fernet
-
-# Generowanie klucza
-key = Fernet.generate_key()
-
-# Zapis klucza do pliku
-with open("secret.key", "wb") as key_file:
-    key_file.write(key)
